64cc3ac7d22b26c3d83784d82f0dd4d91f8bcd74
Guimel <GuimelXD@users.noreply.github.com>
2020-06-10 14:57:31 -0300

    Teste de conexão com o banco

--- app/src/main/java/teste/br/sblendersApp/LoginActivity.java
+++ app/src/main/java/teste/br/sblendersApp/LoginActivity.java
@@ -1,13 +1,22 @@
 package teste.br.sblendersApp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+        Button btnLogin = findViewById(R.id.button2);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                //new UserNameToId().execute("https://uhunt.onlinejudge.org/api/uname2uid/almaruf");
+            }
+        });
+    }*/
 }
