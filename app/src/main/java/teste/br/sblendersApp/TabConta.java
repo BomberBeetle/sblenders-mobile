ec6a8049cb1f65494e69a609c9f842db771d0142
BomberBeetle <gdreks@gmail.com>
2020-09-01 23:17:53 -0300

    logoff funcionando (thanks por me lembrar guimel) e todo adicionado

--- app/src/main/java/teste/br/sblendersApp/TabConta.java
+++ app/src/main/java/teste/br/sblendersApp/TabConta.java
@@ -1,7 +1,9 @@
 package teste.br.sblendersApp;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,14 +15,16 @@ import android.widget.Button;
 public class TabConta extends Fragment {
 
     Button btnLogoff;
-
+    SharedPreferences prefs;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle onCreateSavedInstanceBundle){
         View tabConta = inflater.inflate(R.layout.tab_conta, container, false);
+        prefs = getActivity().getSharedPreferences("login", Context.MODE_PRIVATE);
         btnLogoff = tabConta.findViewById(R.id.btn_logoff);
         btnLogoff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                prefs.edit().remove("id").remove("token").commit();
                 Intent intent = new Intent(v.getContext(), LoginActivity.class);
                 v.getContext().startActivity(intent);
             }
